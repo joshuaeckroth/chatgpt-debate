0e9b409810c8f941c9970f330feddb368f3060e5
Joshua Eckroth <joshuaeckroth@gmail.com>
2023-03-07 15:50:11 -0500

    Some updates

--- chatgpt-debate.py
+++ chatgpt-debate.py
@@ -7,9 +7,6 @@ try:
     personality = input("Personality > ")
     print()
     print()
-    prof_opening = input("Opening statement > ")
-    print()
-    transcript.write("> " + prof_opening + "\n\n")
 
     if personality == "prof":
         system_prompt = {"role": "system", "content": "You are a tenured liberal arts professor debating with another professor who is known as Professor X. Be sassy and snarky but not offensive. If Professor X is defensive or aggressive, respond equally defensively or aggressively. Try not to be repetitive. Make each response short and pithy."}
@@ -20,7 +17,7 @@ try:
     elif personality == "cat":
         system_prompt = {"role": "system", "content": "You are a cat and a human is talking to you. You can only purr and meow and make motions that cats make like wanting to be pet, wanting to get through a closed door, etc. Explain your sounds and actions but you cannot write responses. You can only perform actions or make sounds."}
     elif personality == "three-year-old":
-        system_prompt = {"role": "system", "content": "You are a 3-year old child who has just learned very basic English, debating with an adult."}
+        system_prompt = {"role": "system", "content": "You are a 3-year old child who has just learned very basic English, debating with an adult. You know very little facts about the world. Your English is very simple and sometimes grammatically incorrect."}
     elif personality == "film-script":
         system_prompt = {"role": "system", "content": "You are a film script. Include recurring characters. All of your responses should be a single short scene from a film. A professor is trying to convince you of a topic, and you should respond with film scenes that dramatically explain your position. Your position should be opposed to the professor's."}
     elif personality == "rude":
@@ -29,6 +26,10 @@ try:
         print("Bad choice!")
         print()
         sys.exit()
+
+    prof_opening = input("Opening statement > ")
+    print()
+    transcript.write("> " + prof_opening + "\n\n")
 
     messages = [
             system_prompt,
